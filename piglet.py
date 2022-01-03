7727678f307c3a871b96cfca1ab6e53fad1fe8cf
aaron.hoffer <aaron.hoffer@connexta.com>
2022-01-03 13:52:37 -0700

    Resizeable window, exit app when window closes

--- piglet.py
+++ piglet.py
@@ -1,14 +1,22 @@
 from pyglet.gl import *
 
 # Direct OpenGL commands to this window.
-window = pyglet.window.Window()
+from pyglet.window import Window
 
-label = pyglet.text.Label('Hello, world',
-                          color=(128, 128, 255, 255),
-                          font_name='Times New Roman',
-                          font_size=46,
-                          x=window.width // 2, y=window.height // 2,
-                          anchor_x='center', anchor_y='center')
+window = pyglet.window.Window(resizable=True)
+
+
+# Full screen
+# window = Window(fullscreen=True, config=config)
+
+
+def label():
+    return pyglet.text.Label('Hello, world',
+                             color=(128, 128, 255, 255),
+                             font_name='Times New Roman',
+                             font_size=46,
+                             x=window.width // 2, y=window.height // 2,
+                             anchor_x='center', anchor_y='center')
 
 
 @window.event
@@ -20,7 +28,13 @@ def on_draw():
     glVertex2f(window.width, 0)
     glVertex2f(window.width, window.height)
     glEnd()
-    label.draw()
+    label().draw()
+
+
+@window.event
+def on_close():
+    print("I'm closing now")
+    exit(0)
 
 
 music = pyglet.resource.media('cinematic-space-drone-10623.mp3')
