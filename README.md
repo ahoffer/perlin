21be416d64319fef80baff43e4e8eaa19441bb12
aaron.hoffer <aaron.hoffer@connexta.com>
2022-01-03 18:21:14 -0700

    Decided on pyglet as the OpenGL interface. Next task is draw a triangle mesh in 3D.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # perlin
 Python terrain demo
 
+/projects/venv/bin/pip install perlin-noise pyglet
--- piglet.py
+++ piglet.py
@@ -1,22 +1,23 @@
-from pyglet.gl import *
+import pyglet
+from perlin_noise import PerlinNoise
+from pyglet.gl import glClear, GL_COLOR_BUFFER_BIT, glLoadIdentity, glBegin, GL_TRIANGLES, glVertex2f, glEnd
 
-# Direct OpenGL commands to this window.
-from pyglet.window import Window
+# Not using these lines yet
+display = pyglet.canvas.Display()
+screen = display.get_default_screen()
 
+# Resizeable
 window = pyglet.window.Window(resizable=True)
-
 
 # Full screen
 # window = Window(fullscreen=True, config=config)
 
-
-def label():
-    return pyglet.text.Label('Hello, world',
-                             color=(128, 128, 255, 255),
-                             font_name='Times New Roman',
-                             font_size=46,
-                             x=window.width // 2, y=window.height // 2,
-                             anchor_x='center', anchor_y='center')
+label = pyglet.text.Label('Hello, world',
+                          color=(64, 128, 255, 255),
+                          font_name='Times New Roman',
+                          font_size=46,
+                          x=window.width // 2, y=window.height // 2,
+                          anchor_x='center', anchor_y='center')
 
 
 @window.event
@@ -28,7 +29,8 @@ def on_draw():
     glVertex2f(window.width, 0)
     glVertex2f(window.width, window.height)
     glEnd()
-    label().draw()
+    label.update(window.width // 2, window.height // 2)
+    label.draw()
 
 
 @window.event
@@ -37,9 +39,13 @@ def on_close():
     exit(0)
 
 
+# MUSIC
 music = pyglet.resource.media('cinematic-space-drone-10623.mp3')
-music.play()
+# music.play()
 
-pyglet.app.run()
+# NOISE
+noise = PerlinNoise(octaves=3, seed=1)
+xpix, ypix = 100, 100
+pic = [[noise([i / xpix, j / ypix]) for j in range(xpix)] for i in range(ypix)]
 
 pyglet.app.run()
